0e0a5cd792bce6e6901c587b19597ac55bc56d60
dance2die <dontbothermewithspam@gmail.com>
2016-02-24 10:01:49 -0500

    Tested Enum and displaying it in order using TreeSet

--- src/collections/CollectionsMain.java
+++ src/collections/CollectionsMain.java
@@ -1,10 +1,20 @@
 package collections;
 
+import java.util.Set;
+import java.util.TreeSet;
+
 /**
  * Created by dance2die on 2/24/2016.
  */
+
 public class CollectionsMain {
     public static void main(String[] args){
-
+        Set<Weekday> daysOff = new TreeSet<Weekday>();
+        daysOff.add(Weekday.SUNDAY);
+        daysOff.add(Weekday.MONDAY);
+        daysOff.add(Weekday.THURSDAY);
+        daysOff.add(Weekday.SATURDAY);
+        System.out.println(daysOff);
     }
 }
+
---
+++ src/collections/Weekday.java
@@ -0,0 +1,9 @@
+package collections;
+
+/**
+ * Created by dance2die on 2/24/2016.
+ */
+enum Weekday{
+    SUNDAY, MONDAY, TUESDAY,WEDNESDAY, THURSDAY, FRIDAY, SATURDAY
+}
+
