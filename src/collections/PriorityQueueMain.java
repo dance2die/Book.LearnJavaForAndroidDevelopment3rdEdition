dce08f3451a0f757ffd9d91a7cc7f56b165f2e67
dance2die <dontbothermewithspam@gmail.com>
2016-02-25 10:52:22 -0500

    refactor

--- src/collections/PriorityQueueMain.java
+++ src/collections/PriorityQueueMain.java
@@ -1,29 +1,24 @@
 package collections;
 
-import java.util.Comparator;
 import java.util.PriorityQueue;
 import java.util.Queue;
 
 public class PriorityQueueMain
 {
-    final static int NELEM = 15;
+    final static int ELEMENT_COUNT = 15;
 
     public static void main(String[] args)
     {
-        Comparator<Integer> cmp;
-        cmp = new Comparator<Integer>()
-        {
-            @Override
-            public int compare(Integer e1, Integer e2)
-            {
-                return e2 - e1;
-            }
-        };
-        Queue<Integer> qi = new PriorityQueue<Integer>(NELEM, cmp);
-        for (int i = 0; i < NELEM; i++)
+        Queue<Integer> qi = new PriorityQueue<>(ELEMENT_COUNT, (e1, e2) -> e2 - e1);
+
+        for (int i = 0; i < ELEMENT_COUNT; i++) {
             qi.add((int) (Math.random() * 100));
-        while (!qi.isEmpty())
+        }
+
+        while (!qi.isEmpty()) {
             System.out.print(qi.poll() + " ");
+        }
+
         System.out.println();
     }
 }
