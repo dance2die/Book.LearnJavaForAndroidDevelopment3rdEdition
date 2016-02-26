219068135840c9583d21869b9c029b6434df8412
dance2die <dontbothermewithspam@gmail.com>
2016-02-26 09:27:06 -0500

    Learned ARrayDeque

--- src/collections/CollectionsMain.java
+++ src/collections/CollectionsMain.java
@@ -1,8 +1,6 @@
 package collections;
 
-import java.util.EnumSet;
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.*;
 
 /**
  * Created by dance2die on 2/24/2016.
@@ -10,6 +8,23 @@ import java.util.TreeSet;
 
 public class CollectionsMain {
     public static void main(String[] args){
+//        testCollection();
+        testArrayDeque();
+    }
+
+    private static void testArrayDeque() {
+        Deque<String> stack = new ArrayDeque<String>();
+        String[] weekdays = { "Sunday", "Monday", "Tuesday", "Wednesday",
+                "Thursday", "Friday", "Saturday" };
+
+        for (String weekday: weekdays)
+            stack.push(weekday);
+
+        while (stack.peek() != null)
+            System.out.println(stack.pop());
+    }
+
+    private static void testCollection() {
         Set<Weekday> daysOff = new TreeSet<Weekday>();
         daysOff.add(Weekday.SUNDAY);
         daysOff.add(Weekday.MONDAY);
