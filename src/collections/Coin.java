eb585ffab8e24b7d050eae67034ae4a0a2a2110c
dance2die <dontbothermewithspam@gmail.com>
2016-02-28 18:26:38 -0500

    Learned about EnumMap

---
+++ src/collections/Coin.java
@@ -0,0 +1,8 @@
+package collections;
+
+/**
+ * Created by dance2die on 2/28/2016.
+ */
+public enum Coin {
+    PENNY, NICKEL, DIME, QUARTER
+}
--- src/collections/CollectionsMain.java
+++ src/collections/CollectionsMain.java
@@ -11,7 +11,19 @@ public class CollectionsMain {
 //        testCollection();
 //        testArrayDeque();
 //        testColorMap();
-        testWeakHashMap();
+//        testWeakHashMap();
+        testEnumMap();
+    }
+
+    private static void testEnumMap() {
+        Map<Coin, Integer> map = new EnumMap<Coin, Integer>(Coin.class);
+        map.put(Coin.PENNY, 1);
+        map.put(Coin.NICKEL, 5);
+        map.put(Coin.DIME, 10);
+        map.put(Coin.QUARTER, 25);
+        System.out.println(map);
+        Map<Coin,Integer> mapCopy = new EnumMap<Coin, Integer>(map);
+        System.out.println(mapCopy);
     }
 
     private static void testWeakHashMap() {
