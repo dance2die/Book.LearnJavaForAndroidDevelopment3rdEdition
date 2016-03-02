e53919e2bc2c2865e87f7646d413948f8f22ad43
dance2die <dontbothermewithspam@gmail.com>
2016-03-02 09:10:58 -0500

    tested directory and file listing

--- src/io/IOMain.java
+++ src/io/IOMain.java
@@ -1,12 +1,31 @@
 package io;
 
 import java.io.File;
+import java.io.FilenameFilter;
 import java.io.IOException;
 
 public class IOMain {
     public static void main(String[] args){
 //        testListingDrives();
-        testPathNameInfo();
+//        testPathNameInfo();
+        testListingDirectories();
+    }
+
+    private static void testListingDirectories() {
+        File file = new File("c:\\windows");
+        FilenameFilter fnf = new FilenameFilter()
+        {
+            @Override
+            public boolean accept(File dir, String name)
+            {
+                return name.endsWith(".exe");
+            }
+        };
+
+        String[] names = file.list(fnf);
+        for (String name: names) {
+            System.out.println(name);
+        }
     }
 
     private static void testPathNameInfo() {
